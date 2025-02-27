e14cc2438b7c011c54b0eae1c16741106c8d6c05
Shubhankar1211 <155457074+Shubhankar1211@users.noreply.github.com>
2025-02-27 10:08:33 +0000

    Learning conversion and Operators

--- 01_basic/03_Conversion_Operators.js
+++ 01_basic/03_Conversion_Operators.js
@@ -20,3 +20,44 @@ console.log(booleanIsLooggedIn);
 // 1=> true; 0=> false;
 //"" => false
 // " hitesh "=>true 
+
+
+let value = 3;
+let negvalue  = -value;
+console.log(negvalue);//-3
+
+let str1 = "shubhankar";
+let str2 = "chauhan";
+let str3 = str1 + str2;
+console.log(str3);//shubhankarchauhan
+
+console.log("1" + 2); //12
+console.log(1 + "2");//12
+console.log("1" + 2 + 2);//122
+console.log(1 + 2 + "2");//32
+console.log(true);//true
+console.log(+true); 1
+//console.log(true+);//error
+console.log(+""); 0
+
+// postfix operator
+let x = 3;
+const y = x++;
+// x is 4; y is 3
+
+let x2 = 3n;
+const y2 = x2++;
+// x2 is 4n; y2 is 3n
+
+
+let a = 3;//preffix operator
+const b = ++x;
+// x is 4; y is 4
+
+let a2 = 3n;
+const b2 = ++x2;
+// x2 is 4n; y2 is 4n
+
+
+
+
