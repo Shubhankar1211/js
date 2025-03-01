2b1a3ae70721ed8ca1bfe8daa0a0c8b05338577b
Shubhankar1211 <155457074+Shubhankar1211@users.noreply.github.com>
2025-03-01 15:41:42 +0000

    some more modfication

--- 02_basic/02_Arrays.js
+++ 02_basic/02_Arrays.js
@@ -15,3 +15,16 @@ console.log(allFriends); // it return [ 'ayush', 'udit', 'prateek', 'soumya', 'u
 const allFriends = [...school_friends, ...college_friends];
 console.log(allFriends);// same result as above
 
+const array0 = [1,2,3,[4,5,6,],7,[5,7,[4,5]]];
+const array1 = array0.flat(Infinity);
+console.log(array1);
+
+console.log(Array.isArray("Shubhankar chauhan"));
+console.log(Array.from("Shubhankar chauhan"));// from create the array
+console.log(Array.from({name:"shubhankar"})); //it retrun empty array [] we do not determine what is it // interview problem
+
+let score1 = 100;
+let score2 = 200;
+let score3 = 200;
+
+console.log(Array.of(score1 ,score2, score3)); // it also make array
