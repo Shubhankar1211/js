0f802e2c47fb04b0572a5fda93446d631ef5879f
Shubhankar1211 <155457074+Shubhankar1211@users.noreply.github.com>
2025-03-05 16:38:25 +0000

    some addtional changes

--- 02_basic/03_Object.js
+++ 02_basic/03_Object.js
@@ -5,3 +5,13 @@ let obj = {
     branch: " ece-1",
 }
 console.log(obj);
+
+let obj1 = {
+    name : "utkarsh kumar sinha",
+    branch : " ece-1",
+}
+console.log(obj1);
+
+let obj3 = obj + obj1;
+console.log(obj3);
+console.log(typeof obj3); // string
