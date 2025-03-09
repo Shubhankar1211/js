6e1bb70ce89e4dcce0dcc9c3d14ad5370a48106d
Shubhankar1211 <155457074+Shubhankar1211@users.noreply.github.com>
2025-03-09 08:50:41 +0000

    basic function done

---
+++ 03_basic/01_functions.js
@@ -0,0 +1,42 @@
+// here  we built a basic function
+function gretting(){
+    console.log("Good Morning");
+}
+
+gretting // it only refernce of a function 
+gretting() // it is basically execution of the function
+
+
+function calculatar(number1 ,number2){ // these are parameters 
+    console.log(number1+number2);
+}
+
+calculatar();// if we run this it show the nan we have'nt pass any arguemnt in it
+calculatar(4,6);//10
+calculatar(4,"6");//46 // so it nsometimes creates the probelem beacuse of daattype for this over coem ewe dirctly use if condition
+calculatar(4,"a");//4a
+calculatar(4,null); //4
+
+
+function calculatar(number1 ,number2){ // if we want ot return the r result we can use thsi method 
+    let result = (number1+number2);
+    return result; // return karne ke add kuch bhi ye console.log ya retun nahi karega 
+    console.log("hello");
+    // return  numebr1 + number 2 ;
+}
+
+const result = calculatar(10,10);
+console.log("result : " , result); // if we think it store the result no it return the undefiend hame console log se store nahikar skate uske liye return likhna padta h
+
+function gamer(username){
+    if(!username){
+        console.log("please enter your username beacuse you have not entered it")
+        return
+    }
+    return `${username} you are loggedin`;
+}
+gamer("deadly0g"); //it will give none beacuse you ahve not print the fucntion it only return thr function
+console.log(gamer("deadly0g"));
+console.log(gamer("")); // output you are loggedin
+console.log(gamer()); //  undefined you are loggedin
+
