145b6358a4f3ef0489508c6dcf90a91e43776b5b
Shubhankar1211 <155457074+Shubhankar1211@users.noreply.github.com>
2025-03-22 17:17:32 +0000

    completed while loop and do while loops

---
+++ 05_iterations/02_while.js
@@ -0,0 +1,18 @@
+let i =0 ;
+while (i<=20) {
+    console.log(`value of index is ${i}`);
+    i++;
+}
+
+let myarray = [1,2,3,4];
+let arr =0;
+while(arr<myarray.length){
+    console.log(`value is ${myarray[arr]}`);
+    arr++;
+}
+
+let score = 11
+do{
+    console.log(`score is${score}`)
+    score++
+} while(score<=10);
