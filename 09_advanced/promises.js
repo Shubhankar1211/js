42c22b3dae507736f3a0b7efff385ca8f2ddc2a2
Shubhankar1211 <155457074+Shubhankar1211@users.noreply.github.com>
2025-03-29 08:06:36 +0000

    promise , then catch fucntion have been learned

--- 09_advanced/promises.js
+++ 09_advanced/promises.js
@@ -1,1 +1,85 @@
 // firstly we will learn how to make or create the promises
+
+// a promise is an object representing the eventual completion or failure of an asynchronous operations. 
+// p promise is a returned object to which you can attach callbacks isnetesd of passing callbacks into a fucntion
+//
+//
+//
+
+const promiseOne = new Promise(function(resolve, reject){
+     //Do an async task
+     //DB calls , crpto ,network 
+     setTimeout(function(){
+        console.log('async task is completed')
+        resolve() // yha call karne se ye phle promise print hoga fir consumed hoga 
+     },1000)
+    // resolve() // agar hane ye scope ke bahr declare kar diya to phle hi promise consumed ho jayega uske baad promise print hoga 
+}) 
+promiseOne.then(function(){
+    console.log("promise consumed") // this romise will not be consumed untill we don note call the resolve because then and resolve has to ve connected 
+})
+
+
+
+// this is the second way
+new Promise(function(resolve,reject){
+      setTimeout(function(){
+        console.log('second async task is completed')
+        resolve()
+      },1000)
+}).then(function(){
+    console.log('async 2 is resolved')
+})
+
+
+// this is the way how we get the data 
+new Promise((resolve, reject) => {
+    setTimeout(function(){
+        resolve({name:"shubhankar"})
+    },1000)
+}).then(function(user){
+    console.log(user)
+})
+
+
+
+//
+new Promise(function(resolve,reject){
+    setInterval(function(){
+       let error = false
+       if(!error){
+        resolve({name:"utkarsh"})
+       }
+       else{
+        reject('ERROR: something went wrong')
+       }
+    },1000)
+}).then(function(user){
+       console.log(user);
+       return user.name
+}).then(function(name){
+    console.log(name);
+    
+}).catch(function(error){
+    console.log(error);
+}).finally(function(){
+    console.log("the promise is either resolve or rejected")
+})
+//console.timeLog(name) // through this way we cant assess the name 
+
+
+
+//  
+const promiseFive = new Promise((resolve, reject) => {
+    setTimeout(function(){
+        let error = true 
+        if(!error){
+            resolve({name:"soumya", branch : "ece"})
+        }
+        else{
+            reject("ERROR, js went wrong")
+        }
+    },1000)
+})
+
+promiseFive.then()
